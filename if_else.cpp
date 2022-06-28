302c37474769dded04de83f68c3a16d45f93e657
BalaVignesh <reddybalavignesh9979@gmail.com>
2022-06-29 00:20:54 +0530

    "Initial Commit"

---
+++ if_else.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    if (n>0){
+        cout << "The Given Number is positive" << endl;
+    }
+
+    else {
+        cout << "The Given Number is negative" << endl;
+    }
+}
